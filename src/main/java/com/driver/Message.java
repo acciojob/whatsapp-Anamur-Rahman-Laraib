f637d26d733acff931165419341ff884d942aa45
unknown <anamurrahmanlaraib@gmail.com>
2023-01-28 17:46:11 +0530

    try 3

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -12,10 +12,10 @@ public class Message {
     public Message() {
     }
 
-    public Message(int id, String content, Date timestamp) {
+    public Message(int id, String content) {
         this.id = id;
         this.content = content;
-        this.timestamp = timestamp;
+        //this.timestamp = timestamp;
     }
 
 
